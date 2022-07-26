52cd33bde94c7c7e5ba82ddda6ef8df09da0e33f
broccoli <metacog@kakao.com>
2022-07-26 09:59:50 +0900

    Article Entity 설정, 인덱스 설정, getter/setter 설정

--- src/main/java/com/example/springbootboard/domain/Article.java
+++ src/main/java/com/example/springbootboard/domain/Article.java
@@ -1,12 +1,32 @@
 package com.example.springbootboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
-    private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto increment 설정 : IDENTITY 방식으로 설정
+    private Long id;   // id는 setter에 의한 것이 아닌 자동으로 셋팅됨
+
+    // Setter는 각 필드에 걸기 : 클래스 레벨에서 걸지 않기, 특정 필드만 셋팅 가능하게 하려는 의도임
+    @Setter private String title;
+    @Setter private String content;
+
+    @Setter private String hashtag;
 
     private LocalDateTime createdAt;
     private String createdBy;
