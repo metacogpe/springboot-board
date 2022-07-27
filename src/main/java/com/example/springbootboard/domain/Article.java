ded3d3f8f631de4c6ad9bb18a61e02605aeeb029
broccoli <metacog@kakao.com>
2022-07-28 08:43:24 +0900

    엔터티 : Articl, ArticleComment, hibernate가 table생성

--- src/main/java/com/example/springbootboard/domain/Article.java
+++ src/main/java/com/example/springbootboard/domain/Article.java
@@ -3,6 +3,10 @@ package com.example.springbootboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -23,13 +27,21 @@ public class Article {
     private Long id;   // id는 setter에 의한 것이 아닌 자동으로 셋팅됨
 
     // Setter는 각 필드에 걸기 : 클래스 레벨에서 걸지 않기, 특정 필드만 셋팅 가능하게 하려는 의도임
-    @Setter private String title;
-    @Setter private String content;
+    @Setter @Column(nullable = false) private String title;
+    @Setter @Column(nullable = false, length = 10000) private String content;
 
     @Setter private String hashtag;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+    protected Article() {}  // arg 없는 생성자 만들기 : 롬복으로도 가능
+
+    public Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
 }
--- src/main/java/com/example/springbootboard/domain/ArticleComment.java
+++ src/main/java/com/example/springbootboard/domain/ArticleComment.java
@@ -1,14 +1,57 @@
 package com.example.springbootboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
     public class ArticleComment {
-    private Long id;
-    private Article article;  // 게시글 객체 : ArticleComment 는 Article객체와 관계를 맺는 형태
-    private String content;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // auto increment 설정 : IDENTITY 방식으로 설정
+    private Long id;
+    @Setter @ManyToOne(optional = false) private Article article;  // optional false:필수값, 게시글 객체 : ArticleComment 는 Article객체와 관계를 맺는 형태
+    @Setter @Column(nullable = false, length = 500) private String content;
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+    protected ArticleComment() {}
+
+    public ArticleComment(Article article, String content) {}
+
+    // factory method
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    // 생성 기능 이용해서 skeleton 코드 생성
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
