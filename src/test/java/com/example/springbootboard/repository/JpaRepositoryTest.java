ec380dbe8ce6aeca90d55e3bba401ff27fb99fc8
broccoli <metacog@kakao.com>
2022-07-30 08:26:46 +0900

    JPA 테스트 구성과 생성자 주입 패턴

---
+++ src/test/java/com/example/springbootboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example.springbootboard.repository;
+
+import com.example.springbootboard.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.*;  // Jnuit5 대신에 assertj를 더 선호
+
+@DisplayName("JPA 연결 테스")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+
+
+
+}
