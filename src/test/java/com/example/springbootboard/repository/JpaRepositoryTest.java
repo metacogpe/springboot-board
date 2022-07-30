c6d5fb13e3e60e928d75dc618454f6352cc97d34
broccoli <metacog@kakao.com>
2022-07-30 10:05:22 +0900

    data.sql에 테스트 데이터 생성하여 테스트 : mockaroo 활용

--- src/test/java/com/example/springbootboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/springbootboard/repository/JpaRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.example.springbootboard.repository;
 
 import com.example.springbootboard.config.JpaConfig;
+import com.example.springbootboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;  // Jnuit5 대신에 assertj를 더 선호
 
@@ -22,6 +26,22 @@ class JpaRepositoryTest {
     ) {
         this.articleRepository = articleRepository;
         this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+
+        // given
+
+        // when
+        List<Article> articles = articleRepository.findAll();
+
+        // then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(123);
+
     }
 
 
