a469dedf31a1cee3aa8fb116e5585fae2b7fc2c2
broccoli <metacog@kakao.com>
2022-08-03 09:49:49 +0900

    DB 접근과 개발 편의를 위한 부트 프로퍼티 적용

--- src/test/java/com/example/springbootboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/springbootboard/repository/JpaRepositoryTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;  // Jnuit5 대신에 assertj를 더 선호
 
-@DisplayName("JPA 연결 테스")
+//@ActiveProfiles("testdb")
+@DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
