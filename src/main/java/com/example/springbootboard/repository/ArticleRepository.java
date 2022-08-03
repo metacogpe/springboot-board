ffb856b3e668066326bf6bea7581d28c01e51dd9
broccoli <metacog@kakao.com>
2022-08-03 09:58:57 +0900

    @Repository 어노테이션 생략 가능

--- src/main/java/com/example/springbootboard/repository/ArticleRepository.java
+++ src/main/java/com/example/springbootboard/repository/ArticleRepository.java
@@ -3,5 +3,6 @@ package com.example.springbootboard.repository;
 import com.example.springbootboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// @Repository 어노테이션 추가하지 않아도 됨 : JpaRepository의 구현체인 SimpleJpaRepository에 이미 추가되어 있음
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
