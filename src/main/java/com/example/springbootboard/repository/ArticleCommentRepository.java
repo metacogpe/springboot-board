366c0ae3b77bb5910b6a5348084f343aeb08b88c
broccoli <metacog@kakao.com>
2022-08-05 10:06:20 +0900

    data rest 환경 설정

--- src/main/java/com/example/springbootboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/springbootboard/repository/ArticleCommentRepository.java
@@ -2,7 +2,9 @@ package com.example.springbootboard.repository;
 
 import com.example.springbootboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // @Repository 어노테이션 추가하지 않아도 됨 : JpaRepository의 구현체인 SimpleJpaRepository에 이미 추가되어 있음
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
