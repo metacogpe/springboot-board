97ab72c1484faa0c3a15f719feb20894ec0b2b1a
broccoli <metacog@kakao.com>
2022-09-06 10:10:41 +0900

    Thymeleaf decoupled 설정

--- src/main/java/com/example/springbootboard/SpringbootBoardApplication.java
+++ src/main/java/com/example/springbootboard/SpringbootBoardApplication.java
@@ -2,7 +2,9 @@ package com.example.springbootboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan    // thymeleaf3Properties 스캔
 @SpringBootApplication
 public class SpringbootBoardApplication {
 
